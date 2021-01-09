80ee5404b220ab154f73cb2f8d0eb5c629c1fe61
A H Zahin <azahinhasan@gmail.com>
2021-01-09 09:49:43 +0600

    a

--- txtTOvoice.py
+++ txtTOvoice.py
@@ -1,6 +1,7 @@
 import pyttsx3
-
+import speech_recognition as sr
 import os
+
 os.system('cmd /c "pip3 install pyttsx3"')
 print()
 print("-------------------------------------")
@@ -15,12 +16,39 @@ def speak(txt):
     #engine.say(txt)
 
 
-def main():
+def TextToVoice():
     print("This Program will help you by reading any text!!")
     print("Paste your Text:")
     print()
     text = input()
     speak(text)
+
+def VoiceToText():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Speak Anything :")
+        audio = r.listen(sr.Microphone())
+            try:
+                text = r.recognize_google(audio)
+                print("You said : {}".format(text))
+            except:
+                print("Sorry could not recognize what you said")
+
+
+def wrongInput():
+    print("Wrong Input!")
+
+    
+def main():
+    option = input("Chose Your Option: 1> TestToVoice , 2> VoiceToText")
+    
+    if option == 1:
+        TextToVoice()
+    elif option == 2:
+        VoiceToText()
+    else:
+        wrongInput()
+        
 
 
 
